b4f92ed16ff8a8173024b33702a7680162fbfefb
Mwrench <mateus.alves.quintela@gmail.com>
2019-12-09 17:25:22 +0000

    Add project files.

---
+++ euromilhoes/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace euromilhoes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int nestrela = 0, nnumero = 0;
+        private const string cruz = "x";
+        private euromilhoes E = new euromilhoes();
+        
+        private void estrela(object sender, EventArgs e)
+        {
+            Label tmp = (Label)sender;
+            tmp.Text = E.Estrela(Convert.ToInt32(tmp.Tag)-1);
+            teste.Text = tmp.Tag.ToString();
+        }
+
+        private void numeros(object sender, EventArgs e)
+        {
+            Label tmp = (Label)sender;
+            tmp.Text = E.Numeros(Convert.ToInt32(tmp.Tag)-1);
+            teste.Text = E.listanum;
+
+        }
+    }
+}
---
+++ euromilhoes/euromilhoes.cs
@@ -0,0 +1,57 @@
+﻿namespace euromilhoes
+{
+    class euromilhoes
+    {
+        private int nestrela = 0, nnumero = 0;
+        private const string cruz = "x";
+        private bool[] numeros = new bool[50];
+        private bool[] estrela = new bool[12];
+
+        public string Numeros(int n)
+        {
+            if (numeros[n])
+            {
+                numeros[n] = false;
+                nnumero--;
+            }
+            else if (nnumero < 5)
+            {
+                numeros[n] = true;
+                nnumero++;
+            }
+
+            if (numeros[n]) return cruz;
+            else return "";
+        }
+        public string Estrela(int n)
+        {
+            if (estrela[n])
+            {
+                estrela[n] = false;
+                nestrela--;
+            }
+            else if (nestrela < 5)
+            {
+                estrela[n] = true;
+                nestrela++;
+            }
+
+            if (estrela[n]) return cruz;
+            else return "";
+        }
+        public string listanum
+        {
+            get
+            {
+                string lista = "";
+                for (int i = 0; i < 50; i++)
+                    if (numeros[i])
+                    {
+                        if (lista != "") lista += "+";
+                        lista += (i + 1).ToString();
+                    }
+                return lista;
+            }
+        }
+    }
+}
